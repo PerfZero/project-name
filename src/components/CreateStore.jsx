3ab74d81623cad0049512710880b619d91ed346a
PerfZero1 <gundenis1@gmail.com>
2024-09-17 12:56:59 +0300

    next

--- src/components/CreateStore.jsx
+++ src/components/CreateStore.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import Step1 from './steps/Step1';
 import Step2 from './steps/Step2';
@@ -7,7 +7,8 @@ import Step4 from './steps/Step4';
 import Step5 from './steps/Step5'; // Импортируйте новый компонент Step5
 
 const CreateStore = ({ setShowFooter }) => {
-  const [formData, setFormData] = React.useState({
+  const [currentStep, setCurrentStep] = useState(1);
+  const [formData, setFormData] = useState({
     userType: '',
     storeType: '',
     theme: '',
@@ -36,14 +37,31 @@ const CreateStore = ({ setShowFooter }) => {
     };
   }, [setShowFooter]);
 
+  const handleBack = () => {
+    if (currentStep > 1) {
+      setCurrentStep(currentStep - 1);
+      navigate(`/create-store/step${currentStep - 1}`);
+    } else {
+      // Если это первый шаг, можно перенаправить на предыдущую страницу или сделать что-то другое
+      window.history.back();
+    }
+  };
+
+  const handleNext = () => {
+    if (currentStep < 5) {
+      setCurrentStep(currentStep + 1);
+      navigate(`/create-store/step${currentStep + 1}`);
+    }
+  };
+
   return (
     <div>
       <Routes>
-        <Route path="step1" element={<Step1 formData={formData} setFormData={setFormData} />} />
-        <Route path="step2" element={<Step2 formData={formData} setFormData={setFormData} />} />
-        <Route path="step3" element={<Step3 formData={formData} setFormData={setFormData} />} />
-        <Route path="step4" element={<Step4 formData={formData} setFormData={setFormData} />} />
-        <Route path="step5" element={<Step5 formData={formData} setFormData={setFormData} />} /> {/* Новый маршрут для Step5 */}
+        <Route path="step1" element={<Step1 formData={formData} setFormData={setFormData} onBack={handleBack} onNext={handleNext} />} />
+        <Route path="step2" element={<Step2 formData={formData} setFormData={setFormData} onBack={handleBack} onNext={handleNext} />} />
+        <Route path="step3" element={<Step3 formData={formData} setFormData={setFormData} onBack={handleBack} onNext={handleNext} />} />
+        <Route path="step4" element={<Step4 formData={formData} setFormData={setFormData} onBack={handleBack} onNext={handleNext} />} />
+        <Route path="step5" element={<Step5 formData={formData} setFormData={setFormData} onBack={handleBack} onNext={handleNext} />} /> {/* Новый маршрут для Step5 */}
       </Routes>
     </div>
   );

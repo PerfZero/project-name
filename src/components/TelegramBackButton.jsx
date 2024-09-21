c2683978f27d217e9d63d477535405bce8aa2d94
PerfZero1 <gundenis1@gmail.com>
2024-09-21 18:34:58 +0300

    ds

--- src/components/TelegramBackButton.jsx
+++ src/components/TelegramBackButton.jsx
@@ -4,25 +4,19 @@ function TelegramBackButton() {
   useEffect(() => {
     const tg = window.Telegram.WebApp;
 
-    const isHomePage = window.location.pathname === '/store/'; // Проверка на главную страницу
-
-    if (isHomePage) {
-      if (!tg.BackButton.isVisible) {
-        tg.BackButton.show();
-      }
-
-      const handleBackButtonClick = () => {
-        window.history.back(); // Логика возврата на предыдущую страницу
-      };
-
-      tg.BackButton.onClick(handleBackButtonClick);
-
-      return () => {
-        tg.BackButton.offClick(handleBackButtonClick);
-      };
-    } else {
-      tg.BackButton.hide(); // Скрыть кнопку на других страницах
+    if (!tg.BackButton.isVisible) {
+      tg.BackButton.show();
     }
+
+    const handleBackButtonClick = () => {
+      window.history.back(); // Логика возврата на предыдущую страницу
+    };
+
+    tg.BackButton.onClick(handleBackButtonClick);
+
+    return () => {
+      tg.BackButton.offClick(handleBackButtonClick);
+    };
   }, []);
 
   return null;
